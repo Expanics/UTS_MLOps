34bc54e290e7353a15ab639726c937b4c0b1c216
Expancis <ofc.rza@gmail.com>
2025-11-16 19:51:39 +0700

    fix

--- app.py
+++ app.py
@@ -304,4 +304,4 @@ def initialize_app():
 
 if __name__ == '__main__':
     initialize_app()
-    app.run(debug=True, host='0.0.0.0', port=5001)  
+    app.run(debug=True, host='0.0.0.0', port=8080)  
